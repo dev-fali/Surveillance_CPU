b461d1a341172eee32b10727a60aead9ff4b90f7
faiz92000 <111262408+faiz92000@users.noreply.github.com>
2024-07-04 14:15:12 +0200

    Ajout couleur de fond

--- resource_monitor.py
+++ resource_monitor.py
@@ -14,13 +14,14 @@ import getpass
 CPU_THRESHOLD = int(input("Veuillez entrer le seuil d'alerte pour le CPU (en %): "))
 MEMORY_THRESHOLD = int(input("Veuillez entrer le seuil d'alerte pour la mémoire (en %): "))
 DISK_THRESHOLD = int(input("Veuillez entrer le seuil d'alerte pour le disque (en %): "))
+send_email_alerts = input("Souhaitez-vous activer les alertes par email ? (Oui/Non): ").lower() == 'oui'
 
-alert_email = input("Veuillez entrer l'adresse e-mail pour les alertes: ")
-
-smtp_server = input("Veuillez entrer l'adresse du serveur SMTP: ")
-smtp_port = int(input("Veuillez entrer le port du serveur SMTP: "))
-smtp_username = input("Veuillez entrer votre nom d'utilisateur SMTP: ")
-smtp_password = getpass.getpass("Veuillez entrer votre mot de passe SMTP: ")
+if send_email_alerts:
+    alert_email = input("Veuillez entrer l'adresse e-mail pour les alertes: ")
+    smtp_server = input("Veuillez entrer l'adresse du serveur SMTP: ")
+    smtp_port = int(input("Veuillez entrer le port du serveur SMTP: "))
+    smtp_username = input("Veuillez entrer votre nom d'utilisateur SMTP: ")
+    smtp_password = getpass.getpass("Veuillez entrer votre mot de passe SMTP: ")
 
 def send_email_alert(subject, message):
     msg = MIMEText(message)
@@ -51,8 +52,9 @@ def monitor_resources():
 
         update_gui(cpu_usage, memory_usage, disk_usage)
 
-        if cpu_usage > CPU_THRESHOLD:
-            send_email_alert('CPU Alert', f'CPU usage is {cpu_usage}%')
+        if send_email_alerts:
+            if cpu_usage > CPU_THRESHOLD:
+                send_email_alert('CPU Alert', f'CPU usage is {cpu_usage}%')
         if memory_usage > MEMORY_THRESHOLD:
             send_email_alert('Memory Alert', f'Memory usage is {memory_usage}%')
         if disk_usage > DISK_THRESHOLD:
@@ -82,12 +84,29 @@ def update_gui(cpu, memory, disk):
     ax_memory.plot(memory_values, label='Memory Usage')
     ax_disk.plot(disk_values, label='Disk Usage')
 
+    # Changer la couleur des graphiques si les seuils sont dépassés
+    if cpu > CPU_THRESHOLD:
+        ax_cpu.set_facecolor('red')
+    else:
+        ax_cpu.set_facecolor('white')
+        
+    if memory > MEMORY_THRESHOLD:
+        ax_memory.set_facecolor('red')
+    else:
+        ax_memory.set_facecolor('white')
+        
+    if disk > DISK_THRESHOLD:
+        ax_disk.set_facecolor('red')
+    else:
+        ax_disk.set_facecolor('white')
+
     ax_cpu.legend(loc='upper right')
     ax_memory.legend(loc='upper right')
     ax_disk.legend(loc='upper right')
 
     canvas.draw()
 
+# Initialisation de l'interface graphique
 root = tk.Tk()
 root.title('System Resource Monitor')
 
